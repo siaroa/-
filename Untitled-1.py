0720144a334e891e6bc3686053f10461a93334dd
siaroa <97201789+siaroa@users.noreply.github.com>
2022-01-06 09:02:11 +0900

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,18 @@
+import discord, asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready(): # 봇이 실행되면 한 번 실행됨
+    print("이 문장은 Python의 내장 함수를 출력하는 터미널에서 실행됩니다\n지금 보이는 것 처럼 말이죠")
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("!명령어"))
+
+@client.event
+async def on_message(message):
+    if message.content == "::::": # 메세지 감지
+        await message.channel.send ("{} | {}, Hello".format(message.author, message.author.mention))
+        await message.author.send ("{} | {}, User, Hello".format(message.author, message.author.mention))
+
+# 봇을 실행시키기 위한 토큰을 작성해주는 곳
+client.run('OTI4NDMwMzU3NTA4OTMxNjM0.YdYqJg.RoxtIFL6Nm0YeT9f5VMrLsx4bt8')
+
